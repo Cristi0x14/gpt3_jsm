b61aced2910d05600975ea8f7aad9d39e9de8796
Cristi0x14 <vasile.bledea@student.upt.ro>
2023-10-24 00:23:45 +0300

    Added nav bar, header, mail box ,bg image and more

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
-//import './App.css';
+import './App.css';
 
 const App = () => (
   <div className="App">
     <div className="gradient__bg">
-      <Navbar></Navbar>
+      <Navbar />
       <Header />
     </div>
     <Brand />
