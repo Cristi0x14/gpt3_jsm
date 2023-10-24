b76484cbcde96b577c843e127875ac76658fc78a
Cristi0x14 <vasile.bledea@student.upt.ro>
2023-10-24 03:45:33 +0300

    fixy fixy

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css';
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand section_padding'>
+      <div>
+        <img src={google} alt='google'></img>
+      </div>
+      <div>
+        <img src={shopify} alt='shopify'></img>
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox'></img>
+      </div>
+      <div>
+        <img src={slack} alt='slack'></img>
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian'></img>
+      </div>
     </div>
   )
 }
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      WhatGpt3
+    <div className="gpt3__whatgpt3 section__padding" id="wgpt3">
+      <div className='gpt3__whatgpt3-bg'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature /> 
       </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+      </div>
+    </div>
   )
 }
 
